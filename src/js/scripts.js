f46be6899c3d5a42a9142673f4d491c9d4f4d8a9
Yaroslava <yaroslava.hovenko@gmail.com>
2024-10-14 23:58:49 +0300

    add play audio

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,10 +3,13 @@ import { BASE_URL } from "./constants.js";
 const input = document.getElementById("word-input");
 const form = document.querySelector(".form");
 const resWord = document.querySelector(".res-word");
+const soundContainer = document.querySelector(".res-sound");
 
 //*in order to value will be accessible
 let data = {
   word: "",
+  meanings: [],
+  phonetics: [],
 };
 
 const insertWord = () => {
@@ -29,12 +32,44 @@ const handleSubmit = async (evt) => {
     const value = await res.json(); //response have json method which help unpack data from res if status = ok
 
     if (res.ok && value.length) {
+      //кладемо дані щоб були доступні кругом (і лише перший елемент)
+      const item = value[0];
+
+      //копіємо глобальний об'єкт з даними
+      data = {
+        ...data,
+        meanings: item.meanings,
+        phonetics: item.phonetics,
+      };
+
       insertWord();
     }
   } catch (error) {
     console.log(error);
   }
 };
+const handleSound = () => {
+  //якщо взагалі шось є у phonetics
+  if (data.phonetics.length) {
+    const sound = data.phonetics[0];
+
+    //*зробити окремо uk and us sound
+    if (sound.audio) {
+      new Audio(sound.audio).play();
+    }
+  }
+};
 
 input.addEventListener("keyup", handleFindValue);
 form.addEventListener("submit", handleSubmit);
+soundContainer.addEventListener("click", handleSound);
+
+// const audioPlayer = document.getElementById("audioPlayer");
+
+// soundContainer.addEventListener("click", () => {
+//   if (data.phonetics.length) {
+//     const sound = data.phonetics[0];
+//     audioPlayer.src = sound.audio;
+//     audioPlayer.play();
+//   }
+// });
